c05d70e34e4312996b584fe1244be6c9fb593472
naritotakizawa <toritoritorina@gmail.com>
2020-01-17 19:40:14 +0900

    詳細ページの戻るリンクを、トップへ戻る固定のリンクに。ヘッダーを無くすので、その兼ね合い

--- nblog1/templatetags/nblog1.py
+++ nblog1/templatetags/nblog1.py
@@ -55,16 +55,3 @@ def render_subscribe_section():
     if settings.USE_WEB_PUSH:
         context['ONE_SIGNAL_APP_ID'] = settings.ONE_SIGNAL_APP_ID
     return context
-
-
-@register.simple_tag
-def get_return_link(request):
-    top_page = resolve_url('nblog1:top')
-    referer = request.environ.get('HTTP_REFERER')
-    # URL直接入力やお気に入りアクセスのときはリファラがないので、トップぺージに戻す
-    if referer:
-        # リファラがある場合、前回ページが自分のサイト内であれば、そこに戻す。
-        parse_result = parse.urlparse(referer)
-        if request.get_host() == parse_result.netloc:
-            return referer
-    return top_page
